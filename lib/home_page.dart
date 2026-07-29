7d7c4c8b0f937e8652264d919163b9e00a076ff3
Shafa Anjoolaoluwa <shafaanjola@gmail.com>
2026-07-29 21:55:39 +0100

    Start statistics screen and learn bottom navigation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -23,6 +23,7 @@ class Transaction {
 
 class _HomePageState extends State<HomePage> {
   double balance = 100000.00;
+  int currentScreen = 0;
   List<Transaction> transactions = [];
   String formatCurrency(double amount) {
     return '₦${amount.toStringAsFixed(2)}';
@@ -103,159 +104,281 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  double _totalIncome() {
+    double total = 0;
+    for (Transaction t in transactions) {
+      if (t.isDeposit) {
+        total = total + t.amount;
+      }
+    }
+    return total;
+  }
+
+  double _totalExpense() {
+    double total = 0;
+    for (Transaction t in transactions) {
+      if (!t.isDeposit) {
+        total = total + t.amount;
+      }
+    }
+    return total;
+  }
+
+  Widget _buildHomeContent() {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 12),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              elevation: 4,
+              color: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(255, 13, 84, 62),
+                      Color.fromARGB(255, 7, 67, 48),
+                      Color.fromARGB(255, 3, 40, 28),
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.account_balance_wallet,
+                        size: 32,
+                        color: Color(0XFFD1FAE5),
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        formatCurrency(balance),
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          color: const Color.fromARGB(255, 6, 38, 6),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12.0,
+                  vertical: 24,
+                ),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    _showAmountSheet(true);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 150, 203, 185),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 12,
+                    ),
+                  ),
+                  icon: Icon(Icons.south_east),
+                  label: Text('Deposit'),
+                ),
+              ),
+              SizedBox(width: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12.0,
+                  vertical: 24.0,
+                ),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    _showAmountSheet(false);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 150, 203, 185),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 12,
+                    ),
+                  ),
+                  icon: Icon(Icons.north_east),
+                  label: Text('Withdraw'),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (context, index) {
+                Transaction t = transactions[index];
+                return Card(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 6,
+                    horizontal: 8,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                        color: t.isDeposit ? Colors.green : Colors.red,
+                        width: 2,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        children: [
+                          Icon(
+                            t.isDeposit ? Icons.south_east : Icons.north_east,
+                            color: t.isDeposit ? Colors.green : Colors.red,
+                          ),
+                          SizedBox(width: 12),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(formatCurrency(t.amount)),
+                              Text(t.description),
+                              Text(t.date.toString()),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileContent() {
+    return const Center(child: Text('Profile page coming soon'));
+  }
+
+  Widget _buildStatisticsContent() {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Card(
+              elevation: 4,
+              color: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Container(
+                height: 200,
+                color: Color.fromARGB(255, 3, 40, 28),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.south_east),
+                    Text(
+                      formatCurrency(_totalIncome()),
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Card(
+              elevation: 4,
+              color: const Color.fromARGB(0, 4, 48, 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Container(
+                height: 200,
+                color: Color.fromARGB(255, 13, 84, 62),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.north_east),
+                    Text(
+                      formatCurrency(_totalExpense()),
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPlanningContent() {
+    return const Center(child: Text('Planning page coming soon'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(height: 12),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                elevation: 4,
-                color: Colors.transparent,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Container(
-                  height: 200,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color.fromARGB(255, 131, 207, 132),
-                        Color.fromARGB(255, 92, 186, 106),
-                        Color(0xFF10B981),
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ),
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.account_balance_wallet,
-                          size: 32,
-                          color: Color(0XFFD1FAE5),
-                        ),
-                        SizedBox(width: 8),
-                        Text(
-                          formatCurrency(balance),
-                          style: Theme.of(context).textTheme.titleLarge
-                              ?.copyWith(
-                                color: const Color.fromARGB(255, 6, 38, 6),
-                              ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+      body: switch (currentScreen) {
+        0 => _buildHomeContent(),
+        1 => _buildStatisticsContent(),
+        2 => _buildPlanningContent(),
+        3 => _buildProfileContent(),
+        _ => _buildHomeContent(),
+      },
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(24),
+          topRight: Radius.circular(24),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: currentScreen,
+          backgroundColor: Color.fromARGB(255, 150, 203, 186),
+          selectedItemColor: Color.fromARGB(255, 35, 81, 49),
+          unselectedItemColor: Color(0xFF10B981),
+          onTap: (index) {
+            setState(() {
+              currentScreen = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart),
+              label: 'Statistics',
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12.0,
-                    vertical: 24,
-                  ),
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      _showAmountSheet(true);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 150, 203, 185),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                        vertical: 12,
-                      ),
-                    ),
-                    icon: Icon(Icons.south_east),
-                    label: Text('Deposit'),
-                  ),
-                ),
-                SizedBox(width: 16),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12.0,
-                    vertical: 24.0,
-                  ),
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      _showAmountSheet(false);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 150, 203, 185),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                        vertical: 12,
-                      ),
-                    ),
-                    icon: Icon(Icons.north_east),
-                    label: Text('Withdraw'),
-                  ),
-                ),
-              ],
+            BottomNavigationBarItem(
+              icon: Icon(Icons.event_note),
+              label: 'Planning',
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: transactions.length,
-                itemBuilder: (context, index) {
-                  Transaction t = transactions[index];
-                  return Card(
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 6,
-                      horizontal: 8,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(
-                          color: t.isDeposit ? Colors.green : Colors.red,
-                          width: 2,
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(12),
-                        child: Row(
-                          children: [
-                            Icon(
-                              t.isDeposit ? Icons.south_east : Icons.north_east,
-                              color: t.isDeposit ? Colors.green : Colors.red,
-                            ),
-                            SizedBox(width: 12),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(formatCurrency(t.amount)),
-                                Text(t.description),
-                                Text(t.date.toString()),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
           ],
         ),
       ),
